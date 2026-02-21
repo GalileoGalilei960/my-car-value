6ab1d7248de7ea2af94035be1d90109cd7ef2744
SwelteBonsai960 <haladiyostap@gmail.com>
2026-02-21 09:39:03 +0200

    sync commit

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,52 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    ParseIntPipe,
+    Patch,
+    Post,
+    Query,
+} from '@nestjs/common';
 import { CreateUserDto } from 'src/dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { GetUserByEmailDto } from 'src/dtos/get-user-by-email-query.dto';
+import { UpdateUserDto } from 'src/dtos/update-user.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from 'src/dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
     constructor(private userService: UsersService) {}
     @Post()
     async signUp(@Body() body: CreateUserDto) {
-        const user = await this.userService.createUser(
-            body.email,
-            body.password,
-        );
+        const user = await this.userService.create(body.email, body.password);
 
         return user;
+    }
+
+    @Get('/:id')
+    async getUserById(@Param('id', ParseIntPipe) id: number) {
+        return this.userService.findOne(id);
+    }
+
+    @Get()
+    async getUserByEmail(@Query() query: GetUserByEmailDto) {
+        return this.userService.find(query.email);
+    }
+
+    @Delete('/:id')
+    async deleteUserById(@Param('id', ParseIntPipe) id: number) {
+        return this.userService.delete(id);
+    }
+
+    @Patch('/:id')
+    async updateUserById(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() body: UpdateUserDto,
+    ) {
+        return this.userService.update(id, body);
     }
 }
