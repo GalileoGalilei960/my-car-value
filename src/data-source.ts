f063f0205468493a7df3b90ad85128874dd9cb9c
GalileoGalilei960 <haladiyostap@gmail.com>
2026-03-07 17:20:39 +0200

    hope this will be deleted

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,8 +1,30 @@
 import { DataSource } from 'typeorm';
+import { DataSourceOptions } from 'typeorm/browser';
 
-export const appDataSource = new DataSource({
-    type: 'sqlite',
-    database: 'db.sqlite',
+const config = {
     entities: ['**/*.entity.ts'],
     migrations: [__dirname + '/migrations/*.ts'],
-});
+};
+
+switch (process.env.NODE_ENV) {
+    case 'development':
+        Object.assign(config, {
+            type: 'sqlite',
+            database: 'db.sqlite',
+        });
+        break;
+    case 'test':
+        Object.assign(config, {
+            type: 'sqlite',
+            database: 'test.sqlite',
+        });
+        break;
+    case 'production':
+        Object.assign(config, {
+            type: 'postgres',
+            url: process.env.DATABASE_URL,
+        });
+        break;
+}
+
+export const appDataSource = new DataSource(config as DataSourceOptions);
