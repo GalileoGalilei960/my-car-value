9cf9a1c171a5a526fa97cc7c3891658ca82d01a8
GalileoGalilei960 <haladiyostap@gmail.com>
2026-03-07 15:06:45 +0200

    adding different envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/users.entity';
-import { Report } from './reports/reports.entity';
 import { APP_PIPE } from '@nestjs/core';
 import cookieSession from 'cookie-session';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmConfigService } from './config/typeorm.config';
 
 @Module({
     imports: [
@@ -19,16 +18,10 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
             envFilePath: `.${process.env.NODE_ENV || 'development'}.env`,
         }),
         TypeOrmModule.forRootAsync({
-            inject: [ConfigService],
-            useFactory: (config: ConfigService) => {
-                return {
-                    type: 'sqlite',
-                    database: config.get<string>('DB_NAME') || 'db.sqlite',
-                    entities: [User, Report],
-                    synchronize: true,
-                };
-            },
+            useClass: TypeOrmConfigService,
         }),
+        UsersModule,
+        ReportsModule,
     ],
     controllers: [AppController],
     providers: [
@@ -42,13 +35,16 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     ],
 })
 export class AppModule {
+    constructor(private configService: ConfigService) {}
     configure(consumer: MiddlewareConsumer) {
         consumer
             .apply(
                 cookieSession({
-                    keys: ['secret temporary keys'],
+                    keys: [
+                        this.configService.get<string>('COOKIE_KEY') ||
+                            'default-key',
+                    ],
                 }),
-                // CurrentUserMiddleware,
             )
             .forRoutes('*');
     }
